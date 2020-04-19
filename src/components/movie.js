908e935bbd60bb8638659ea8f6aace6a22bffe6d
brotherRED2077 <60204044+brotherRED2077@users.noreply.github.com>
2020-04-19 15:47:43 -0600

    updated app

--- src/components/movie.js
+++ src/components/movie.js
@@ -12,7 +12,7 @@ class Movie extends Component {
     componentDidMount() {
         const {dispatch} = this.props;
         if (this.props.selectedMovie == null) {
-            dispatch(fetchMovie(this.props.movieId));
+            dispatch(fetchMovie(this.props.title));
         }
     }
 
@@ -25,10 +25,10 @@ class Movie extends Component {
             )
         }
 
-        const ReviewInfo = ({reviews}) => {
+        const ReviewInfo = ({movieReviews}) => {
             return reviews.map((review, i) =>
                 <p key={i}>
-                    <b>{review.reviewerName}</b> {review.review}
+                    <b>{review.reviewerName}</b> {review.quote}
                     <Glyphicon glyph={'star'} /> {review.rating}
                 </p>
             )
@@ -47,7 +47,7 @@ class Movie extends Component {
                       <ListGroupItem><ActorInfo actors={currentMovie.actors} /></ListGroupItem>
                       <ListGroupItem><h4><Glyphicon glyph={'star'}/> {currentMovie.avgRating} </h4></ListGroupItem>
                   </ListGroup>
-                  <Panel.Body><ReviewInfo reviews={currentMovie.reviews} /></Panel.Body>
+                  <Panel.Body><ReviewInfo reviews={currentMovie.movieReviews} /></Panel.Body>
               </Panel>
             );
         }
@@ -62,7 +62,7 @@ const mapStateToProps = (state, ownProps) => {
     console.log(ownProps);
     return {
         selectedMovie: state.movie.selectedMovie,
-        movieId: ownProps.match.params.movieId
+        title: ownProps.match.params.title
     }
 }
 
